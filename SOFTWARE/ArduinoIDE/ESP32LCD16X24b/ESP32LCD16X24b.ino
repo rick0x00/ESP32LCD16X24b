2e16fb7606eeb972d700ab2420e58eba75b6c195
silvajhb <jhbs.student@gmail.com>
2021-09-22 22:41:39 -0300

    add files]

---
+++ SOFTWARE/ArduinoIDE/ESP32LCD16X24b/ESP32LCD16X24b.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 23, rw = 4, en = 22, d4 = 5, d5 = 18, d6 = 19, d7 = 21;
+LiquidCrystal lcd(rs, rw, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
